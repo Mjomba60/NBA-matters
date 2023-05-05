1027e7544ffdd98c8d29de7697e25e2aebb17b2a
George Samuel <geosam2708@gmail.com>
2023-05-05 13:40:00 +0300

    added nav links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Switch} from "react-router-dom"
+import NavBar from './components/NavBar';
+import Players from "./components/Players";
+import Teams from "./components/Teams";
+import Games from "./components/Games"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        NBA Matters
       </header>
+      <NavBar />
+      <Switch>
+        <Route path ="/teams">
+          <Teams />
+        </Route>
+        <Route path='/games'>
+          <Games />
+        </Route>
+        <Route exact path = "/">
+          <Players />
+        </Route>
+      </Switch>
     </div>
   );
 }
