bde85a702e41bbece29d85ae82f9bf598014352b
George Samuel <geosam2708@gmail.com>
2023-05-05 13:41:09 +0300

    Added teams along with data

---
+++ src/components/Teams.js
@@ -0,0 +1,19 @@
+import React, {useEffect, useState} from "react";
+
+export default function Teams(){
+    const [teams, setTeams] = useState([])
+
+    useEffect(() => {
+        fetch("https://www.balldontlie.io/api/v1/teams")
+        .then(r => r.json())
+        .then(resp => setTeams(resp.data))
+    },[])
+
+    const team = teams.map(t => {
+        return <p key={t.id}>{t.full_name}</p>
+    })
+
+    return <div id="teams">
+            {team}
+            </div>
+}
