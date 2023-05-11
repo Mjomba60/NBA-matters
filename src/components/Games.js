3718039f96e7150144f83e65511cef0e2ee076d6
George Samuel <geosam2708@gmail.com>
2023-05-11 17:35:27 +0300

    added click listenre to indivdual games

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,40 +1,27 @@
 import React, { useEffect, useState } from "react";
 
- export default function Games(){
+ export default function Games({selectObj, selectOrigin}){
     const [games, setGames] = useState([])
     
-
+    //fetch data from server and store for later use
     useEffect(()=>{
         fetch("https://www.balldontlie.io/api/v1/games")
         .then(r => r.json())
         .then(resp => setGames(resp.data))
     }, [])
 
-    const game = games.map(g => {
-        return <dl key={g.id}>
-        <dt>
-            <label>home team score : visitor team score </label>
-            {g.home_team_score} : {g.visitor_team_score}
-        </dt>
-        <dd>
-            <label>Name : </label>
-            {g.home_team.full_name}  vs  {g.visitor_team.full_name}
-        </dd>
-        <dd>
-            <label>period : </label>
-            {g.period}</dd>
-        <dd>
-            <label>status : </label>
-            {g.status}
-        </dd>
-        <dd>
-        <label>postSeason : </label>
-            {g.postSeason}
-        </dd>
-    </dl>
+
+    //iterate through data from server displaying item from the data
+    const game = games.map(snGame => {
+        return <div className="matches" key={snGame.id} onClick = {() => {selectObj(snGame)
+                selectOrigin("games")}}>
+                <p>{snGame.home_team.name} vs {snGame.visitor_team.name}</p>
+                <p className="time"><strong>date : </strong>{snGame.date}</p>
+                </div>
     })
 
-    return <div>
+    return <div id="games">
+            <h3>Matches</h3>
             {game}
             </div>
  } 
