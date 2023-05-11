c9084d069f325debcf4c8df36e87f884cfffbe71
George Samuel <geosam2708@gmail.com>
2023-05-11 17:36:28 +0300

    added proper handling of props and passing

--- src/App.js
+++ src/App.js
@@ -4,26 +4,44 @@ import NavBar from './components/NavBar';
 import Players from "./components/Players";
 import Teams from "./components/Teams";
 import Games from "./components/Games"
+import Details from './components/Details';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [obj, setObj] = useState({})
+  const [origin, setOrigin] = useState("")
+  useEffect(()=> {setObj(obj)},[obj])
+
+
+  //grab object to be used by the details info component
+  function selectObj(object){
+    setObj({...object})
+  }
+
+  //grab origiof the request where data was received from
+  function selectOrigin(root){
+    setOrigin(root)
+  }
   return (
     <div className="App">
       <header className="App-header">
         NBA Matters
       </header>
       <NavBar />
+      <div className='searchNdetails'>
       <Switch>
-        <Route path ="/teams">
-          <Teams />
+        <Route exact path ="/teams">
+          <Teams selectObj ={selectObj} selectOrigin = {selectOrigin}/>
         </Route>
-        <Route path='/games'>
-          <Games />
+        <Route exact path='/games'>
+          <Games selectObj ={selectObj} selectOrigin = {selectOrigin}/>
         </Route>
-        <Route exact path = "/">
-          <Players />
+        <Route  path = "/">
+          <Players selectObj ={selectObj} selectOrigin = {selectOrigin}/>
         </Route>
       </Switch>
-      
+      <Details obj={obj} origin ={origin}/>
+      </div>
     </div>
   );
 }
